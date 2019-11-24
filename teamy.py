12547db5d6a79fe14e0a520ddb4c1e10ce6b8bbc
yasmineskalli <57916482+yasmineskalli@users.noreply.github.com>
2019-11-24 15:09:46 -0500

    Update teamy.py

--- teamy.py
+++ teamy.py
@@ -2,13 +2,107 @@ from turtle import *
 y = Turtle()
 colormode(255)
 
-def reminder():
-  # by Mr. Riley
-  # don't forget to put a comment with your name at the top of each function you make
-  # this will help to keep track of who created each function
-  return True
+def WriteText (centerX, centerY,text, color):
+  #made by yasmine!
+  y.penup()
+  centerX=int(centerX)
+  centerY=int(centerY)
+  y.goto(centerX,centerY)
+  y.color(color)
+  y.write(text)
   
+def DrawRect(topLeftX, topLeftY, width, height, color):
+  #made by yasmineeee
+  topLeftX=int(topLeftX)
+  topLeftY=int(topLeftY)
+  height=int(height)
+  width=int(width)
+  y.penup()
+  y.goto(topLeftX, topLeftY)
+  y.setheading(0)
+  y.color(color)
+  y.begin_fill()
+  y.pendown()
+  for i in range(2):
+    y.forward(width)
+    y.right(90)
+    y.forward(height)
+    y.right(90)
+  y.end_fill()
+  y.penup()
+  
+def window():
+  #made by yasmine, not an object on its own
+  y.pendown()
+  y.forward(8)
+  y.right(90)
+  y.fillcolor("white")
+  y.begin_fill()
+  y.forward(8)
+  y.right(90)
+  y.forward(8)
+  y.right(90)
+  y.forward(8)
+  y.right(90)
+  y.penup()
+  y.end_fill()
+  
+def makeRowOfWindows():
+    for i in range (3):
+      window()
+      y.forward(12)
+      y.pendown()
+    y.penup()
+    y.right(90)
+    y.forward(16)
+    y.right(90)
+    y.forward(36)
+    y.right(180)
 
+def skyscraper():
+  #made by yasmine
+  y.fillcolor("black")
+  y.begin_fill()
+  y.pendown()
+  y.forward(45)
+  y.right(90)
+  y.forward(300)
+  y.right(90)
+  y.forward(45)
+  y.right(90)
+  y.forward(300)
+  y.right(180)
+  y.pendown()
+  y.forward(25)
+  y.end_fill()
+  y.penup()
+  y.left(90)
+  y.forward(6)
+  for i in range (16):
+   makeRowOfWindows()
+
+ 
+def bus():
+  #made by yasmine 
+  y.forward(350)
+  y.left(90)
+  y.forward(100)
+  y.left(90)
+  y.forward(350)
+  y.left(90)
+  y.forward(100)
+  y.penup()
+  y.left(90)
+  y.forward(40)
+  y.pendown()
+  for i in range(360):
+    y.forward(.75)
+    y.right(1)
+  y.forward(250)
+  for i in range(360):
+    y.forward(.75)
+    y.right(1)
+  
 
 
 update()
