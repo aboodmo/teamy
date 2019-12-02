ec0de9ec10a36f24fff6b301a11c29b1685744ce
aboodmo <57917560+aboodmo@users.noreply.github.com>
2019-12-02 14:09:37 -0500

    Update teamy.py

--- teamy.py
+++ teamy.py
@@ -170,37 +170,37 @@ def bus():
   y.penup()
   
   
-  def stickPerson():
-  #made by yasmine
-  #head and body
-    y.pendown()
-    y.pencolor("gray")
-    y.fillcolor("gray")
-    y.begin_fill()
-    for i in range(360):
-      y.forward(.5)
-      y.left(1)
-    y.end_fill()
-    y.right(90)
-    y.forward(125)
-    #legs
-    y.left(45)
-    y.forward(85)
-    y.right(180)
-    y.forward(85)
-    y.left(90)
-    y.forward(85)
-    # arms
-    y.right(180)
-    y.forward(85)
-    y.left(45)
-    y.forward(50)
-    y.right(90)
-    y.forward(85)
-    y.left(180)
-    y.forward(85)
-    y.forward(85)
-    y.penup()
+def stickPerson():
+#made by yasmine
+#head and body
+  y.pendown()
+  y.pencolor("gray")
+  y.fillcolor("gray")
+  y.begin_fill()
+  for i in range(360):
+    y.forward(.5)
+    y.left(1)
+  y.end_fill()
+  y.right(90)
+  y.forward(125)
+  #legs
+  y.left(45)
+  y.forward(85)
+  y.right(180)
+  y.forward(85)
+  y.left(90)
+  y.forward(85)
+  # arms
+  y.right(180)
+  y.forward(85)
+  y.left(45)
+  y.forward(50)
+  y.right(90)
+  y.forward(85)
+  y.left(180)
+  y.forward(85)
+  y.forward(85)
+  y.penup()
 
 def fish():
   #made by yasmine
@@ -392,5 +392,157 @@ def pumpkin():
   y.end_fill()
   y.penup()
 
+def make_Boat():
+  #by Abood
+  bob.fillcolor('saddle brown')
+  bob.begin_fill()
+  bob.setheading(-85)
+  for i in range(50):
+      bob.forward(2)
+      bob.left(1)
+  bob.left(25)
+  for i in range(60):
+      bob.forward(5)
+      bob.left(1)
+  bob.left(25)
+  for i in range(20):
+      bob.forward(3)
+      bob.left(1.5)
+  bob.left(120)
+  for i in range(43):
+      bob.forward(8)
+      bob.right(1.55)
+  bob.right(120)
+  for i in range(48):
+      bob.forward(7)
+      bob.right(1.1)
+  bob.right(2)
+  bob.end_fill()
+  bob.fillcolor('chocolate')
+  bob.begin_fill()
+  bob.right(120)
+  for i in range(48):
+      bob.forward(7)
+      bob.right(1.3)
+  bob.right(135)
+  for i in range(30):
+      bob.forward(9)
+      bob.right(.75)
+  bob.end_fill()
+
+  def make_seaweed():
+    #by abood
+  bob.fillcolor('dark green')
+  bob.begin_fill()
+  bob.setheading(135)
+  for i in range(70):
+      bob.forward(1.5)
+      bob.right(1)
+  for i in range(70):
+      bob.forward(1.5)
+      bob.left(1)
+  for i in range(70):
+      bob.forward(1.5)
+      bob.right(1)
+  bob.right(190)
+  for i in range(70):
+      bob.forward(1.5)
+      bob.left(1)
+  for i in range(70):
+      bob.forward(1.5)
+      bob.right(1)
+  for i in range(70):
+      bob.forward(1.5)
+      bob.left(1)
+  bob.left(90)
+  bob.forward(10)
+  bob.end_fill()
+
+  def make_kite():
+    #by abood
+  bob.setheading(-90)
+  bob.fillcolor('blue')
+  bob.begin_fill()
+  bob.forward(100)
+  bob.left(90)
+  bob.forward(5)
+  bob.left(90)
+  bob.forward(100)
+  bob.right(45)
+  bob.forward(50)
+  for i in range(2):
+      bob.left(90)
+      bob.forward(53)
+  bob.left(90)
+  bob.forward(50)
+  bob.end_fill()
+
+  def make_cloud():
+    #by abood
+  bob.fillcolor('white')
+  bob.begin_fill()
+  bob.setheading(90)
+  bob.forward(10)
+  for i in range(180):
+      bob.forward(1)
+      bob.right(1)
+  bob.right(180)
+  for i in range(180):
+      bob.forward(1)
+      bob.right(1)
+  bob.right(180)
+  for i in range(180):
+      bob.forward(1)
+      bob.right(1)
+  bob.forward(20)
+  for i in range(90):
+      bob.forward(1)
+      bob.right(1)
+  bob.forward(230)
+  for i in range(98):
+      bob.forward(1)
+      bob.right(1)
+  bob.end_fill()
+
+  def make_UFO():
+    #by abood
+  bob.setheading(180)
+  bob.fillcolor('gray')
+  bob.begin_fill()
+  for i in range(145):
+      bob.forward(1)
+      bob.left(1)
+  for i in range (35):
+      bob.forward(5)
+      bob.left(1)
+  for i in range(145):
+      bob.forward(1)
+      bob.left(1)
+  for i in range (35):
+      bob.forward(5)
+      bob.left(1)
+  bob.end_fill()
+  bob.fillcolor('white')
+  bob.begin_fill()
+  bob.penup()
+  bob.left(90)
+  bob.forward(50)
+  bob.pendown()
+  bob.left(165)
+  bob.forward(15)
+  for i in range (12):
+      bob.forward(6)
+      bob.right(4)
+  for i in range (44):
+      bob.forward(4)
+      bob.right(3)
+  bob.forward(35)
+  bob.right(45)
+  for i in range (36):
+      bob.forward(5)
+      bob.right(2.80)
+  bob.right(45)
+  bob.forward(5)
+  bob.end_fill()
 
 update()
